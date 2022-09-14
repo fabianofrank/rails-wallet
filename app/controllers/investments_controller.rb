000f120945ec9e633391f7d80dde93ab3f9d8868
fabianofrank <fabianofrank@protonmail.com>
2022-09-14 13:08:33 -0300

    Ascending order for Investments

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -1,6 +1,6 @@
 class InvestmentsController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
-    @investments = Group.find(params[:group_id]).investment
+    @investments = Group.find(params[:group_id]).investment.order(id: :asc)
   end
 end
